90ffe96ded19c1e279c44450be119aedced1ae43
jdbyrd <jd3yrd@gmail.com>
2017-12-05 11:37:48 -0600

    working video list

--- src/views/app.js
+++ src/views/app.js
@@ -3,20 +3,13 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    // let videoList = [];
-    // for (let value of window.exampleVideoData) {
-    //   videoList.push(new Video(value));
-    // } 
     this.videos = new Videos(window.exampleVideoData);
-    this.render();
-    
+    this.render(); 
   },
 
-
   render: function() {
-    let list = new VideoListView({collection: this.videos});
     this.$el.html(this.template());
-    this.$el.append(list.render());
+    new VideoListView({collection: this.videos, el: this.$('.list')}).render();
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,21 +1,20 @@
 var VideoListView = Backbone.View.extend({
 
   initialize: function() {
-    //console.log('videlistview ran');
     this.collection.on('sync', this.render, this);
   },
 
   render: function() {
-    console.log(this.collection);
     this.$el.children().detach();
-    this.$el.html(this.template(this.collection.model));
-    this.collection.forEach(this.renderVideo, this);
+    let videoList = this.collection.map(this.renderVideo, this);
+    this.$el.html(this.template());
+    this.$('.video-list').append(videoList);
     return this.$el;
   },
   
   renderVideo: function(video) {
-    var videoListView = new VideoListEntryView({model: video});
-    this.$el.append(videoListView.render());
+    var videoListView = new VideoListEntryView({model: video}).render();
+    return videoListView;
   },
 
   template: templateURL('src/templates/videoList.html')
